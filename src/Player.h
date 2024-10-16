f8b60c60f0cda4e9ffdc298fd50bb10dfcb70a24
Pedro Omedas <pomedas@gmail.com>
2024-10-16 16:49:25 +0200

    Modified player movement to use only gravity calculated by Box2D

--- src/Player.h
+++ src/Player.h
@@ -39,6 +39,6 @@ public:
 
 	// L08 TODO 5: Add physics to the player - declare a Physics body
 	PhysBody* pbody;
-	float jumpForce = 1.2f; // The force to apply when jumping
+	float jumpForce = 2.5f; // The force to apply when jumping
 	bool isJumping = false; // Flag to check if the player is currently jumping
 };
