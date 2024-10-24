c68038411bb5de2d088b2ab9697aa27c92ebc742
Pedro Omedas <pomedas@gmail.com>
2024-10-24 19:46:18 +0200

    Function to destro bodies in the Physics world. Example in the player

--- src/Physics.h
+++ src/Physics.h
@@ -2,6 +2,7 @@
 #include "Module.h"
 #include "Entity.h"
 #include "box2d/box2d.h"
+#include <list>
 
 #define GRAVITY_X 0.0f
 #define GRAVITY_Y -10.0f
@@ -77,6 +78,8 @@ public:
 	void BeginContact(b2Contact* contact);
 	void EndContact(b2Contact* contact);
 
+	void DeletePhysBody(PhysBody* physBody);
+
 private:
 
 	// Debug mode
@@ -84,4 +87,7 @@ private:
 
 	// Box2D World
 	b2World* world;
+
+	// List of physics bodies
+	std::list<PhysBody*> bodiesToDelete;
 };
