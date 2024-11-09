e0f93bc96b9d90235343a9156f6a00ba0e3e260a
Pedro Omedas <pomedas@gmail.com>
2024-11-09 17:41:13 +0100

    added functions and variables needed to implement Dijkstra algorithm
    - modified ResetPath(), DrawPath()
    - added PropagateDijkstra(), :MovementCost(), ComputePath() and Find()

--- src/Pathfinding.h
+++ src/Pathfinding.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <list>
 #include <queue>
+#include <functional> // for std::greater
 #include "Vector2D.h"
 #include "SDL2/SDL.h"
 #include "Map.h"
@@ -20,17 +21,33 @@ public:
     bool IsWalkable(int x, int y);
     void PropagateBFS();
 
+    // L12: Methods for BFS + Pathfinding and cost function for Dijkstra
+    int MovementCost(int x, int y);
+    void ComputePath(int x, int y);
+    void PropagateDijkstra();
+
+private:
+    int Find(std::vector<Vector2D> vector, Vector2D elem);
+
 public:
 
     // L11: BFS Pathfinding variables
     Map* map;
     MapLayer* layerNav;
     std::queue<Vector2D> frontier;
-    std::list<Vector2D> visited;
+    std::vector<Vector2D> visited;
     SDL_Texture* pathTex = nullptr;
     Vector2D destination;
 
-    //Gid of the tiles that block the path - Important adjust this value to your map
-    int blockedGid = 49; 
+    // L12: Dijkstra Pathfinding variables
+    std::priority_queue<std::pair<int, Vector2D>, std::vector<std::pair<int, Vector2D>>, std::greater<std::pair<int, Vector2D>> > frontierDijkstra;
+    std::vector<Vector2D> breadcrumbs; //list of tiles that form the path
+    std::vector<std::vector<int>> costSoFar; //matrix that stores the accumulated cost in the propagation of the Dijkstra algorithm
+    std::list<Vector2D> pathTiles; //list of tiles that form the path
+    SDL_Texture* tileX = nullptr; //texture used to show the path 
+
+    int blockedGid = 49; //Gid of the tiles that block the path - Important adjust this value to your map
+    int highCostGid = 50; //Gid of the tiles that have high cost - Important adjust this value to your map
+
 };
 
