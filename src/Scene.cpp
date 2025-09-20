191fa69d01333bec54939b93f6380f34d0e8b8ce
Pedro Omedas <pedro@xr.consulting>
2025-09-20 10:40:53 +0200

    restored bg usic and updated calls to smart pointers

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -29,8 +29,8 @@ bool Scene::Awake()
 // Called before the first frame
 bool Scene::Start()
 {
-	img = Engine::GetInstance().textures.get()->Load("Assets/Textures/test.png");
-	Engine::GetInstance().audio.get()->PlayMusic("Assets/Audio/Music/music_spy.ogg");
+	img = Engine::GetInstance().textures->Load("Assets/Textures/test.png");
+	Engine::GetInstance().audio->PlayMusic("Assets/Audio/Music/level-iv-339695.wav");
 	return true;
 }
 
@@ -45,28 +45,28 @@ bool Scene::Update(float dt)
 {
 	//L03 TODO 3: Make the camera movement independent of framerate
 
-	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
-		Engine::GetInstance().render.get()->camera.y -= 1;
+	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		Engine::GetInstance().render->camera.y -= 1;
 
-	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
-		Engine::GetInstance().render.get()->camera.y += 1;
+	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		Engine::GetInstance().render->camera.y += 1;
 
-	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-		Engine::GetInstance().render.get()->camera.x -= 1;
+	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		Engine::GetInstance().render->camera.x -= 1;
 
-	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		Engine::GetInstance().render.get()->camera.x += 1;
+	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		Engine::GetInstance().render->camera.x += 1;
 
 	//Get the size of the window
 	int windowW, windowH;
-	Engine::GetInstance().window.get()->GetWindowSize(windowW, windowH);
+	Engine::GetInstance().window->GetWindowSize(windowW, windowH);
 
 	//Get the size of the texture
 	int texW, texH;
-	Engine::GetInstance().textures.get()->GetSize(img, texW, texH);
+	Engine::GetInstance().textures->GetSize(img, texW, texH);
 
 	// Renders the image in the center of the screen
-	Engine::GetInstance().render.get()->DrawTexture(img, windowW /2 - texW / 2, windowH /2 - texH / 2);
+	Engine::GetInstance().render->DrawTexture(img, windowW /2 - texW / 2, windowH /2 - texH / 2);
 
 	return true;
 }
@@ -76,7 +76,7 @@ bool Scene::PostUpdate()
 {
 	bool ret = true;
 
-	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
 		ret = false;
 
 	return ret;
