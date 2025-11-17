59096ec68cdf4de6454a1eeb52ee9e2e96890d42
Pedro Omedas <pedro.omedas@citm.upc.edu>
2025-11-17 12:28:06 +0100

    FIX: Map coordinates  follows the same convention as Tiled (i,j)
    renamed variables in world to map function
    
    FIX: Colliders load from Tile Layer follows Tiled coordinates convention

--- src/Map.cpp
+++ src/Map.cpp
@@ -42,8 +42,8 @@ bool Map::Update(float dt)
         for (const auto& mapLayer : mapData.layers) {
             //L09 TODO 7: Check if the property Draw exist get the value, if it's true draw the lawyer
             if (mapLayer->properties.GetProperty("Draw") != NULL && mapLayer->properties.GetProperty("Draw")->value == true) {
-				for (int i = 0; i < mapData.height; i++) {
-					for (int j = 0; j < mapData.width; j++) {
+				for (int i = 0; i < mapData.width; i++) {
+					for (int j = 0; j < mapData.height; j++) {
 						// L07 TODO 9: Complete the draw function
                         //Get the gid from tile
                         int gid = mapLayer->Get(i, j);
@@ -184,8 +184,8 @@ bool Map::Load(std::string path, std::string fileName)
         //Iterate the layer and create colliders
         for (const auto& mapLayer : mapData.layers) {
             if (mapLayer->name == "Collisions") {
-                for (int i = 0; i < mapData.height; i++) {
-                    for (int j = 0; j < mapData.width; j++) {
+                for (int i = 0; i < mapData.width; i++) {
+                    for (int j = 0; j < mapData.height; j++) {
                         int gid = mapLayer->Get(i, j);
                         if (gid == 49) {
                             Vector2D mapCoord = MapToWorld(i, j);
@@ -234,12 +234,12 @@ bool Map::Load(std::string path, std::string fileName)
 }
 
 // L07: TODO 8: Create a method that translates x,y coordinates from map positions to world positions
-Vector2D Map::MapToWorld(int i, int j) const
+Vector2D Map::MapToWorld(int x, int y) const
 {
     Vector2D ret;
 
-    ret.setX((float)(j * mapData.tileWidth));
-    ret.setY((float)(i * mapData.tileHeight));
+    ret.setX((float)(x * mapData.tileWidth));
+    ret.setY((float)(y * mapData.tileHeight));
 
     return ret;
 }
