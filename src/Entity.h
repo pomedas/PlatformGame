1f437a61e6fae90c74f7da0ed587335c697f1ded
Pedro Omedas <pedro@xr.consulting>
2025-11-26 14:19:13 +0100

    Fix: Handles entities Destroy at the end of entitiemanagers Update
    Avoids problems when entities are removed in the middle of entitemanager's update

--- src/Entity.h
+++ src/Entity.h
@@ -76,6 +76,7 @@ public:
 	std::string name;
 	EntityType type;
 	bool active = true;
+	bool pendingToDelete = false;
 
 	// Possible properties, it depends on how generic we
 	// want our Entity class, maybe it's not renderable...
