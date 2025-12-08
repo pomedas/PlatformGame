a096c89f583bd63b4495dbdb8fe921297541b2b5
Pedro Omedas <pedro@xr.consulting>
2025-12-08 20:55:51 +0100

    Implements Scene Loading example (Main Menu, Level1 and Level2)

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -27,19 +27,8 @@ Scene::~Scene()
 bool Scene::Awake()
 {
 	LOG("Loading Scene");
+	LoadScene(currentScene); // empieza en MAIN_MENU
 	bool ret = true;
-
-	//L08: TODO 4: Create a new item using the entity manager and set the position to (200, 672) to test
-	std::shared_ptr<Item> item = std::dynamic_pointer_cast<Item>(Engine::GetInstance().entityManager->CreateEntity(EntityType::ITEM));
-	item->position = Vector2D(200, 672);
-
-	//Create a new enemy 
-	std::shared_ptr<Enemy> enemy1 = std::dynamic_pointer_cast<Enemy>(Engine::GetInstance().entityManager->CreateEntity(EntityType::ENEMY));
-	enemy1->position = Vector2D(384, 672);
-
-	// L16: TODO 2: Instantiate a new GuiControlButton in the Scene
-	SDL_Rect btPos = { 520, 350, 120,20 };
-	uiBt = std::dynamic_pointer_cast<UIButton>(Engine::GetInstance().uiManager->CreateUIElement(UIElementType::BUTTON, 1, "MyButton", btPos, this));
 
 	return ret;
 }
@@ -47,17 +36,6 @@ bool Scene::Awake()
 // Called before the first frame
 bool Scene::Start()
 {
-	Engine::GetInstance().audio->PlayMusic("Assets/Audio/Music/level-iv-339695.wav");
-
-	//L06 TODO 3: Call the function to load the map. 
-	Engine::GetInstance().map->Load("Assets/Maps/", "MapTemplate.tmx");
-
-	//L15 TODO 3: Call the function to load entities from the map
-	Engine::GetInstance().map->LoadEntities(player);
-
-	// Texture to highligh mouse position 
-	mouseTileTex = Engine::GetInstance().textures->Load("Assets/Maps/MapMetadata.png");
-	
 	return true;
 }
 
@@ -70,49 +48,214 @@ bool Scene::PreUpdate()
 // Called each loop iteration
 bool Scene::Update(float dt)
 {
-	//L03 TODO 3: Make the camera movement independent of framerate
-	float camSpeed = 1;
-
-	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
-		Engine::GetInstance().render->camera.y -= (int)ceil(camSpeed * dt);
-
-	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
-		Engine::GetInstance().render->camera.y += (int)ceil(camSpeed * dt);
-
-	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-		Engine::GetInstance().render->camera.x -= (int)ceil(camSpeed * dt);
-	
-	if(Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		Engine::GetInstance().render.get()->camera.x += (int)ceil(camSpeed * dt);
-
-	//Get mouse position and obtain the map coordinate
-	Vector2D mousePos = Engine::GetInstance().input->GetMousePosition();
-	Vector2D mouseTile = Engine::GetInstance().map->WorldToMap((int)(mousePos.getX() - Engine::GetInstance().render->camera.x),
-															   (int)(mousePos.getY() - Engine::GetInstance().render->camera.y));
-
-	//Render a texture where the mouse is over to highlight the tile, use the texture 'mouseTileTex'
-	Vector2D highlightTile = Engine::GetInstance().map.get()->MapToWorld((int)mouseTile.getX(), (int)mouseTile.getY());
-	SDL_Rect rect = { 0,0,Engine::GetInstance().map->GetTileWidth(),Engine::GetInstance().map->GetTileHeight() };
-	Engine::GetInstance().render.get()->DrawTexture(mouseTileTex,(int)highlightTile.getX(),(int)highlightTile.getY(),&rect);
-
-	// saves the tile pos for debugging purposes
-	if (mouseTile.getX() >= 0 && mouseTile.getY() >= 0 || once) {
-		tilePosDebug = "[" + std::to_string((int)mouseTile.getX()) + "," + std::to_string((int)mouseTile.getY()) + "] ";
-		once = true;
+	switch (currentScene)
+	{
+	case SceneID::INTRO_SCREEN:
+		break;
+	case SceneID::MAIN_MENU:
+		UpdateMainMenu(dt);
+		break;
+	case SceneID::LEVEL1:
+		UpdateLevel1(dt);
+		break;
+	case SceneID::LEVEL2:
+		UpdateLevel2(dt);
+		break;
 	}
-
-	////If mouse button is pressed modify player position
-	//if (Engine::GetInstance().input.get()->GetMouseButtonDown(1) == KEY_DOWN) {
-	//	player->SetPosition(Vector2D(highlightTile.getX(), highlightTile.getY()));
-	//}
 
 	return true;
 }
 
-// Called each loop iteration
 bool Scene::PostUpdate()
 {
 	bool ret = true;
+
+	switch (currentScene)
+	{
+	case SceneID::INTRO_SCREEN:
+		break;
+	case SceneID::MAIN_MENU:
+		break;
+	case SceneID::LEVEL1:
+		PostUpdateLevel1();
+		break;
+	case SceneID::LEVEL2:
+		break;
+	default:
+		break;
+	}
+
+	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+bool Scene::OnUIMouseClickEvent(UIElement* uiElement)
+{
+	switch (currentScene)
+	{
+	case SceneID::INTRO_SCREEN:
+		break;
+	case SceneID::MAIN_MENU:
+		HandleMainMenuUIEvents(uiElement);
+		break;
+	case SceneID::LEVEL1:
+		break;
+	case SceneID::LEVEL2:
+		break;
+	default:
+		break;
+	}
+
+	return true;
+}
+
+bool Scene::CleanUp()
+{
+	LOG("Freeing scene");
+	UnloadCurrentScene();
+	return true;
+}
+
+Vector2D Scene::GetPlayerPosition()
+{
+	if (player) return player->GetPosition();
+	else Vector2D(0,0);
+}
+
+// *********************************************
+// Scene change functions
+// *********************************************
+
+void Scene::LoadScene(SceneID newScene)
+{
+	auto& engine = Engine::GetInstance();
+
+	switch (newScene)
+	{
+	case SceneID::MAIN_MENU:
+		LoadMainMenu();
+		break;
+
+	case SceneID::LEVEL1:
+		LoadLevel1();
+		break;
+
+	case SceneID::LEVEL2:
+		LoadLevel2();
+		break;
+	}
+}
+
+void Scene::ChangeScene(SceneID newScene)
+{
+	UnloadCurrentScene();
+	currentScene = newScene;
+	LoadScene(currentScene);
+}
+
+void Scene::UnloadCurrentScene() {
+
+	switch (currentScene)
+	{
+	case SceneID::MAIN_MENU:
+		UnloadMainMenu();
+		break;
+
+	case SceneID::LEVEL1:
+		UnloadLevel1();
+		break;
+
+	case SceneID::LEVEL2:
+		UnloadLevel2();
+		break;
+	}
+
+}
+
+// *********************************************
+// MAIN MENU functions
+// *********************************************
+
+void Scene::LoadMainMenu() {
+
+	Engine::GetInstance().audio->PlayMusic("Assets/Audio/Music/retro-gaming-short-248416.wav");
+
+	// Instantiate a UIButton in the Scene
+	SDL_Rect btPos = { 520, 350, 120,20 };
+	std::dynamic_pointer_cast<UIButton>(Engine::GetInstance().uiManager->CreateUIElement(UIElementType::BUTTON, 1, "MyButton", btPos, this));
+}
+
+void Scene::UnloadMainMenu() {
+	// Clean up UI elements related to the main menu
+	Engine::GetInstance().uiManager->CleanUp();	
+}
+
+void Scene::UpdateMainMenu(float dt) {}
+
+void Scene::HandleMainMenuUIEvents(UIElement* uiElement)
+{
+	switch (uiElement->id)
+	{
+	case 1: // Button MyButton
+		LOG("Main Menu: MyButton clicked!");
+		ChangeScene(SceneID::LEVEL1);
+		break;
+	default:
+		break;
+	}
+}
+
+// *********************************************
+// Level 1 functions
+// *********************************************
+
+void Scene::LoadLevel1() {
+
+	Engine::GetInstance().audio->PlayMusic("Assets/Audio/Music/level-iv-339695.wav");
+
+	//Call the function to load the map. 
+	Engine::GetInstance().map->Load("Assets/Maps/", "MapTemplate.tmx");
+
+	//Call the function to load entities from the map
+	Engine::GetInstance().map->LoadEntities(player);
+
+	//Create a new item using the entity manager and set the position to (200, 672) to test
+	std::shared_ptr<Item> item = std::dynamic_pointer_cast<Item>(Engine::GetInstance().entityManager->CreateEntity(EntityType::ITEM));
+	item->position = Vector2D(200, 672);
+	item->Start(); //L17 Important call Start
+
+	//Create a new enemy 
+	std::shared_ptr<Enemy> enemy1 = std::dynamic_pointer_cast<Enemy>(Engine::GetInstance().entityManager->CreateEntity(EntityType::ENEMY));
+	enemy1->position = Vector2D(384, 672);
+	enemy1->Start(); //L17 Important call Start
+}
+
+void Scene::UpdateLevel1(float dt) {
+
+	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_2) == KEY_DOWN) {
+		ChangeScene(SceneID::LEVEL2);
+	}
+
+}
+
+void Scene::UnloadLevel1() {
+
+	// Clean up UI elements related to the Level1
+	auto& uiManager = Engine::GetInstance().uiManager;
+	uiManager->CleanUp();
+
+	// Reset player reference (sets the shared_ptr to nullptr)
+	player.reset();
+
+	// Clean up map and entities
+	Engine::GetInstance().map->CleanUp();
+	Engine::GetInstance().entityManager->CleanUp();
+
+}
+
+void  Scene::PostUpdateLevel1() {
 
 	//L15 TODO 3: Call the function to load entities from the map
 	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_F5) == KEY_DOWN) {
@@ -123,31 +266,41 @@ bool Scene::PostUpdate()
 	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN) {
 		Engine::GetInstance().map->SaveEntities(player);
 	}
-
-	if(Engine::GetInstance().input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
-		ret = false;
-
-	return ret;
 }
 
-// Called before quitting
-bool Scene::CleanUp()
-{
-	LOG("Freeing scene");
+// *********************************************
+// Level 2 functions
+// *********************************************
 
-	return true;
+void Scene::LoadLevel2() {
+
+	Engine::GetInstance().audio->PlayMusic("Assets/Audio/Music/that-8-bit-music-322062.wav");
+
+	//Call the function to load the map. 
+	Engine::GetInstance().map->Load("Assets/Maps/", "MapTemplateLevel2.tmx");
+
+	//Call the function to load entities from the map
+	Engine::GetInstance().map->LoadEntities(player);
 }
 
-// Return the player position
-Vector2D Scene::GetPlayerPosition()
-{
-	return player->GetPosition();
+void Scene::UpdateLevel2(float dt) {
+	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_1) == KEY_DOWN) {
+		ChangeScene(SceneID::LEVEL1);
+	}
 }
 
-bool Scene::OnUIMouseClickEvent(UIElement* uiElement)
-{
-	// L16: TODO 5: Implement the OnGuiMouseClickEvent method
-	LOG("Press Gui Control: %d", uiElement->id);
+void Scene::UnloadLevel2() {
 
-	return true;
+	// Clean up UI elements related to the Level2
+	auto& uiManager = Engine::GetInstance().uiManager;
+	uiManager->CleanUp();
+
+	// Reset player reference (sets the shared_ptr to nullptr)
+	player.reset();
+
+	// Clean up map and entities
+	Engine::GetInstance().map->CleanUp();
+	Engine::GetInstance().entityManager->CleanUp();
+
 }
+
--- src/Scene.h
+++ src/Scene.h
@@ -6,6 +6,16 @@
 
 struct SDL_Texture;
 
+//L17 TODO 1: Define SceneID enum class with INTRO_SCREEN, MAIN_MENU, LEVEL1, LEVEL2
+enum class SceneID
+{
+	INTRO_SCREEN,
+	MAIN_MENU,
+	LEVEL1,
+	LEVEL2
+};
+
+
 class Scene : public Module
 {
 public:
@@ -44,6 +54,30 @@ public:
 	// Handles multiple Gui Event methods
 	bool OnUIMouseClickEvent(UIElement* uiElement);
 
+	// L17 TODO 2: Define functions to handle scene changes
+	void ChangeScene(SceneID newScene);
+	void UnloadCurrentScene();
+	void LoadScene(SceneID newScene);
+
+private:
+
+	// L17 TODO 3: Define specific function for main menu scene: Load, Unload, Handle UI events
+	void LoadMainMenu();
+	void UnloadMainMenu();
+	void UpdateMainMenu(float dt);
+	void HandleMainMenuUIEvents(UIElement* uiElement);
+
+	// L17 TODO 4: Define specific functions for level1 scene: Load, Unload, Update, PostUpdate
+	void LoadLevel1();
+	void UnloadLevel1();
+	void UpdateLevel1(float dt);
+	void PostUpdateLevel1();
+
+	// L17 TODO 5: Define specific functions for level2 scene: Load, Unload, Update
+	void LoadLevel2();
+	void UpdateLevel2(float dt);
+	void UnloadLevel2();
+
 private:
 
 	//L03: TODO 3b: Declare a Player attribute
@@ -54,6 +88,8 @@ private:
 
 	// L16: TODO 2: Declare a UIButton 
 	std::shared_ptr<UIButton> uiBt;
-
 	float volume = 1.0;
+
+	// L17 TODO 1: Current scene attribute with initial value
+	SceneID currentScene = SceneID::MAIN_MENU;
 };
